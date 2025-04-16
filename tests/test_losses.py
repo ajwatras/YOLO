e62ed4308a0fe7da1dd3e85982b362c4f316d25b
ajwatras <ajwatras@outlook.com>
2025-04-16 13:28:13 -0700

    Finished adding Yolo V1 Loss and corresponding tests.

---
+++ tests/test_losses.py
@@ -0,0 +1,101 @@
+import pytest
+import torch
+from utils.losses import YoloV1Loss
+
+def test_categorical_loss():
+    #
+    gt = torch.zeros(1,7,7,15)
+    gt[0,0,0,4] = 1
+    gt[0,0,0,6] = 1
+    predicted = torch.zeros(1,7*7*20)
+    predicted[0,4] = 1
+    predicted[0,11] = 1
+
+    loss = YoloV1Loss(7, 2, 10)
+    print('Running Identical Class Test...')
+    identical_class_loss = loss(predicted, gt)
+    # Test when the classes are identical
+    assert identical_class_loss == 0, f'Identical Class codes result in nonzero loss: {identical_class_loss}'
+    print("Identical Class Test Passed")
+    
+    predicted[0,11] = 0
+    predicted[0,12] = 1
+
+    print('Running Separate Class Test...')
+    separate_class_loss = loss(predicted, gt)
+    # Test when the classes are different
+    assert separate_class_loss == 2., f'Separate Loss should be 2.0, actual: {separate_class_loss}'
+    print('Separate Class Test Passed')
+
+    predicted[0,11] = 0.5
+    predicted[0,12] = 0.5
+    print('Running Split Class Test...')
+    split_class_loss = loss(predicted, gt)
+    print(split_class_loss)
+    # Test when the prediction is split
+    assert split_class_loss == .5, f'Split Loss should be 0.5, actual: {split_class_loss}'
+    print('Split Class Test Passed')
+
+    gt[0,0,0,4] = 0
+    predicted[0,4] = 0
+    print('Running No Object Test...')
+    noobj_class_loss = loss(predicted, gt)
+    # test when no object is present.
+    assert noobj_class_loss == 0., f'No Object Loss should be 0., actual: {noobj_class_loss}'
+    print('No Object Test Passed')
+
+def test_detection_loss():
+    gt = torch.zeros(1,7,7,15)
+    gt[0,0,0,4] = 1
+    gt[0,0,0,0] = 0.1
+    gt[0,0,0,1] = 0.1
+    gt[0,0,0,2] = 0.09
+    gt[0,0,0,3] = 0.09
+
+    predicted = torch.zeros(1,7*7*20)
+    predicted[0,4] = 1
+    predicted[0,0] = 0.1
+    predicted[0,1] = 0.1
+    predicted[0,2] = 0.09
+    predicted[0,3] = 0.09
+    loss = YoloV1Loss(7, 2, 10)
+
+    print('Running Identical Detection Test...')
+    identical_det_loss = loss(predicted, gt)
+    assert identical_det_loss == 0, f'Identical Det Loss resulted in nonzero loss: {identical_det_loss}'
+    print('Identical Detection Test Passed')
+
+    print('Running Missed Detection Test...')
+    predicted[0,4] = 0
+    missed_det_loss = loss(predicted, gt)
+    assert missed_det_loss == 1, f'Missed Det loss should be 1, actual: {missed_det_loss}'
+    print('Missed Detection Test Passed')
+
+    predicted[0,4] = 0
+    predicted[0,160] = 0.1
+    predicted[0,161] = 0.1
+    predicted[0,162] = 0.09
+    predicted[0,163] = 0.09
+    predicted[0,164] = 1
+
+    print('Running Wrong Location Test...')
+    wrong_loc_loss = loss(predicted, gt)
+    assert wrong_loc_loss == 1.5, f'Wrong Location loss should be 1.5, actual: {wrong_loc_loss}'
+    print('Wrong Location Test Passed...')
+
+def test_bbox_loss():
+    gt = torch.zeros(1,7,7,15)
+    gt[0,0,0,4] = 1
+    gt[0,0,0,0] = 0.1
+    gt[0,0,0,1] = 0.1
+    gt[0,0,0,2] = 0.09
+    gt[0,0,0,3] = 0.09
+    predicted = torch.zeros(1,7*7*20)
+    predicted[0,4] = 1
+    predicted[0,0] = 1.1
+    predicted[0,1] = -0.9
+    predicted[0,2] = 0.25
+    predicted[0,3] = 0.25
+    loss = YoloV1Loss(7, 2, 10)
+    bbox_loss = loss(predicted, gt)
+    assert torch.abs(bbox_loss - 2.08) < 1e-10, f'BBox Loss should be 2.08: {bbox_loss}'
--- utils/losses.py
+++ utils/losses.py
@@ -1,6 +1,8 @@
 import torch
 import torchvision
-from misc import intersection_over_union
+import torch.nn as nn
+from .misc import intersection_over_union
+
 
 class YoloV1Loss(nn.Module):
     def __init__(self, grid_size, num_boxes, num_classes):
@@ -19,19 +21,61 @@ class YoloV1Loss(nn.Module):
                                           self.grid_size, 
                                           self.num_boxes * 5 + self.num_classes)
         
+        # Compute Responsible bounding boxes for each grid cell.
         iou_b1 = intersection_over_union(predictions[..., 0:4], target[..., 0:4])
         iou_b2 = intersection_over_union(predictions[..., 5:9], target[..., 0:4])
         ious = torch.cat([iou_b1.unsqueeze(0), iou_b2.unsqueeze(0)], dim = 0)
         iou_maxes, bestbox = torch.max(ious, dim = 0)
+        
+        list_of_confs = [predictions[...,i * 5 + 4].unsqueeze(-1) for i in range(self.num_boxes)]
+        bbox_conf = torch.cat(list_of_confs, dim=3)
+        conf_vals = torch.take_along_dim(bbox_conf, bestbox, dim = -1)
+        
+        bboxes = predictions[...,0:self.num_boxes*5]
+        N,H,W,C = predictions.shape
+        bboxes = bboxes.view(N,H,W,self.num_boxes, 5)
+        bestbox = bestbox.expand(-1,-1,-1,5)
+        
+        selected_bboxes = torch.gather(bboxes, dim=3, index = bestbox.unsqueeze(3)).squeeze(3)
+        
+        class_codes = predictions[...,(self.num_boxes * 5):]
+        object_ind = target[..., 4]
 
-        class_loss = torch.sum(predictions[...,self.num_boxes * 5:] - target[...,6:], -1)       
+        class_loss = torch.sum(object_ind * torch.sum((class_codes - target[...,5:])**2, -1))    
+        
+        detection_weight = self.lambda_no_obj * (1 - object_ind) + object_ind
+        det_loss = torch.sum(detection_weight * (object_ind - selected_bboxes[...,4])**2)
+        print("Selected Bbox 0: ")
+        print(selected_bboxes[0,1,1,:])
+        print("Target 0: ")
+        print(target[0,1,1,:])
+        selected_bboxes[...,2:4] = torch.sqrt(selected_bboxes[...,2:4])
+        print("Selected Bbox: ")
+        print(selected_bboxes[0,1,1,0:4])
+        print(selected_bboxes[...,0:4].shape)
+        target_bbox = target[...,0:4].clone()
+        target_bbox[...,2:4] = torch.sqrt(target_bbox[...,2:4])
+        print("Target: ")
+        print(target_bbox[0,1,1,:])
+        print(target_bbox.shape)
+        bbox_loss = torch.sum(object_ind * torch.sum((selected_bboxes[...,0:4] - target_bbox)**2, dim=-1))
+        print('Diff: ')
+        print((selected_bboxes[0,1,1,0:4] - target_bbox[0,1,1,:])**2)
+        print(torch.sum((selected_bboxes[...,0:4] - target_bbox)**2, dim=-1))
+        print(object_ind)
+
+        print(f'Class Loss: {class_loss}')
+        print(f'Det Loss: {det_loss}')
+        print(f'Bbox Loss: {bbox_loss}')
+
+        return class_loss + det_loss + bbox_loss
         
         loss = 0.0
         for i in range(self.grid_size):
             for j in range(self.grid_size):
                 # Compute whether object is present
-                if (target[i,j,5] == 0):
-                    
+                if (target[i,j,4] == 0):
+                    continue
                 else:
                     # Compute responsible bbox
                     
